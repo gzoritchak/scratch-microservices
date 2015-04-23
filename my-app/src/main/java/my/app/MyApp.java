95afc5d8b7d3ef1e602a5ce97524b8e44b396189
gzoritchak <g.zoritchak@gmail.com>
2015-04-23 17:38:45 +0200

    Ajout CircuitBreaker avec Hystrix.

--- my-app/src/main/java/my/app/MyApp.java
+++ my-app/src/main/java/my/app/MyApp.java
@@ -1,13 +1,17 @@
 package my.app;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MyApp {
 
     public static void main(String args[]) throws Throwable {
@@ -17,13 +21,31 @@ public class MyApp {
 
 @RestController
 class MyServiceRestController {
+
+    @Autowired
+    GreeterClient greeterClient;
+
     @RequestMapping("/")
     String home() {
         String who = "world";
-        String content = "Calling greeter with :: " + who;
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:8081/greeter/greet/" + who,
-                String.class);
-        return content + "<br>" + forEntity.getBody();
+        return "Calling greeter with :: " + who + "<br>"
+                + greeterClient.greet(who);
     }
+
+}
+
+@Component
+class GreeterClient {
+
+    @HystrixCommand(fallbackMethod = "defaultGreet")
+    public String greet(String who) {
+        return new RestTemplate()
+                .getForEntity("http://localhost:8081/greeter/greet/" + who,
+                        String.class).getBody();
+    }
+
+    public String defaultGreet(String who){
+        return who;
+    }
+
 }
